b92bf9bb8331a125ea6aeb0d699b800d473e7fcc
Joshua-E-Weinstein <joshua.e.weinstein@gmail.com>
2021-04-10 22:45:32 -0400

    Converted data to list of dictionaries

--- Application/__main__.py
+++ Application/__main__.py
@@ -1,5 +1,13 @@
+print("Importing Libraries")
+
 import numpy as np
+# import tensorflow as tf
+from json import loads
 
-import tensorflow as tf
+print("Running")
 
-appliancesData = open("../Data/Appliances_5.json", 'r')
+appliancesData = open("Data/Appliances_5.json", 'r')
+dataTable = [loads(line) for line in appliancesData]
+
+for line in dataTable:
+    print(line)
