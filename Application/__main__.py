a6cd3773fab5178f319e6d8e9a1ca68f24076eb5
Joshua-E-Weinstein <joshua.e.weinstein@gmail.com>
2021-04-19 15:56:49 -0400

    Adding model save/load.

--- Application/__main__.py
+++ Application/__main__.py
@@ -1,17 +1,35 @@
-print("Importing Libraries")
+print("Importing Libraries..........")
 
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 from json import loads
 
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
+print("Preparing..........")
+
 maxRating = 5
 batchSize = 512
 highestRating = 5.0
 
-print("Running")
+data = input("Name of the Json file in Data: ")
+output = input("Name of file to save model as in Models folder (No extension): ")
 
-appliancesData = open("Data/Office_Products_5.json", 'r')  # Opens data file.
+print("Running..........")
+
+appliancesData = open("Data/{}".format(data), 'r')  # Opens data file.
 dataTable = [loads(line) for line in appliancesData]  # Converts each line of data into a dictionary.
 
 # Makes list of tuples with the reviews tied to their ratings.
@@ -51,6 +69,9 @@ history = model.fit(trainData.shuffle(round(length*0.3)).batch(batchSize),
                     epochs=10,
                     validation_data=validationData.batch(batchSize),
                     verbose=1)
+
+# Saves the model.
+model.save('Models/{}'.format(output))
 
 # Evaluates the model's accuracy and loss.
 results = model.evaluate(testData.batch(batchSize), verbose=2)
--- Application/ui.py
+++ Application/ui.py
@@ -1,14 +1,29 @@
 from tkinter import *
-from tkinter import scrolledtext, font
+from tkinter import scrolledtext, font, ttk
+import glob
 import random
 
 
 # Button command
 def buttonCommand():
     textInput = reviewInput.get('1.0', 'end-1c')
-    print(textInput)
+    selectedFile = "../Models/" + dropdown.get()
+    print(selectedFile, textInput)
     starNum = random.randint(1, 5)
     stars.config(text='★' * starNum)
+
+
+# Dropdown update
+def dropdownUpdate(*args):
+    dirtyOptions = glob.glob('..\Models\*\\')
+    if dirtyOptions:
+        cleanOptions = []
+        for file in dirtyOptions:
+            cleanOptions.append(file.lstrip('..\Models\\').rstrip('\\'))
+        dropdown['values'] = cleanOptions
+    else:
+        dropdown['values'] = ['']
+
 
 # Create window
 root = Tk()
@@ -30,15 +45,25 @@ frame.place(relx=0, rely=0, relheight=1, relwidth=1)
 title = Label(frame, text='Review Rating Suggester', font=titleFont, bg='white')
 title.place(relx=0.5, rely=0.125, width=500, y=-18, x=-250)
 
-instructions = Label(frame, text='Write a review and we\'ll suggest a rating:', font=instructionsFont, bg='white')
+instructions = Label(frame, text='Write a review and we\'ll suggest its rating:', font=instructionsFont, bg='white')
 instructions.place(relx=0.5, rely=0.25, width=500, y=-30, x=-250)
 
 reviewInput = scrolledtext.ScrolledText(frame, undo=True, padx=10, pady=10, bd=3, relief=GROOVE)
 reviewInput.configure(font=textFont)
 reviewInput.place(relx=0.25, rely=0.25, relheight=0.5, relwidth=0.5)
 
-goButton = Button(frame, text='Go', font=buttonFont, command=buttonCommand)
-goButton.place(relx=0.5, rely=0.75, width=200, y=10, x=-100, height=25)
+buttonsFrame = Frame(root, bd=0, bg='white')
+buttonsFrame.place(relx=0.25, rely=0.75, relwidth=0.5, y=10, height=25)
+
+goButton = Button(buttonsFrame, text='Go', font=buttonFont, relief=GROOVE, command=buttonCommand)
+goButton.place(relx=0, rely=0, relwidth=0.3, height=25)
+
+options = ['']
+dropdown = ttk.Combobox(buttonsFrame, values=options, state="readonly", postcommand=dropdownUpdate)
+dropdownUpdate()
+dropdown.current(0)
+dropdown.bind("<<ComboboxSelected>>", dropdownUpdate)
+dropdown.place(relx=0.7, rely=0, relwidth=0.3, height=25)
 
 stars = Label(frame, text='', font=('bold', 30), fg='#FFA41D', bg='white')
 stars.place(relx=0.5, rely=0.875, width=200, y=-15, x=-100)
